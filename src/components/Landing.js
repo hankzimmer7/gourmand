6b99ee30d1319249c52f86c12c602c7b92a4facd
Hank Zimmer <hankzimmer7@gmail.com>
2018-08-23 18:33:03 -0400

    add basic styling to the main page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Title from './Title';
 
-const Landing = () =>
+const Landing = () => (
 
-<div class="container">
-    <header>
-        Gourmand
-    </header>
+<div class=" container landing-container">
+    <Title />
     <div>
-        <button>Sign In</button>
-        <button>Create an Account</button>
+        <button type="button" className="btn btn-primary">Sign In</button>
+    </div>
+    <div>
+        <button type="button" className="btn btn-primary">Create an Account</button>
     </div>
 </div>
+)
 
 export default Landing;
