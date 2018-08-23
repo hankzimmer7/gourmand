7b712b937747e67e563866712ce6fbe52d955f40
Hank Zimmer <hankzimmer7@gmail.com>
2018-08-23 18:05:57 -0400

    adds some basic html

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () =>
+
+<div class="container">
+    <header>
+        Gourmand
+    </header>
+    <div>
+        <button>Sign In</button>
+        <button>Create an Account</button>
+    </div>
+</div>
+
+export default Landing;
