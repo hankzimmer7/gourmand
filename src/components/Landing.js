9050d4c8b3a2472def0cf930230e0eb3364d032d
Hank Zimmer <hankzimmer7@gmail.com>
2018-08-23 18:43:17 -0400

    adds styling to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,13 +3,15 @@ import Title from './Title';
 
 const Landing = () => (
 
-<div class=" container landing-container">
+<div class="landing-container">
     <Title />
+    <div className="button-container">
     <div>
         <button type="button" className="btn btn-primary">Sign In</button>
     </div>
     <div>
         <button type="button" className="btn btn-primary">Create an Account</button>
+        </div>
     </div>
 </div>
 )
