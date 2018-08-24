9aa950174b692cbe3002ea045c9a667e1c1bee60
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-24 14:37:38 -0400

    adds test for api routing

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // Add routes, both API and view
 // app.use(routes); //Commented out while testing server deployment
 
